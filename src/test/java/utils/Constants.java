4a179b911900b28cd6d4fc8147711ba64f4fe242
Yunus Emre Yıldız <yunus.yildiz@appcent.mobi>
2022-04-01 11:37:54 +0300

    password change

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -7,6 +7,6 @@ public class Constants {
         public static final String WRONG_MAIL = "emreyunusyildiz@mail.bu";
         public static final String EMPTY_MAIL = "";
         public static final String WRONG_PASSWORD = "123123123.";
-        public static final String PASSWORD = "14877AaBb.";
+        public static final String PASSWORD = "truePassword123.";
 
 }
